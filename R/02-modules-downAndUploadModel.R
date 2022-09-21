1ab82d63d6a40cf5a53c323edfc265abde31fd57
Antonia Runge <antonia.runge@inwt-statistics.de>
2022-09-21 21:44:08 +0200

    add tables to check if empty

--- R/02-modules-downAndUploadModel.R
+++ R/02-modules-downAndUploadModel.R
@@ -250,16 +250,41 @@ checkForEmptyTables <- function(values) {
       all(is.na(x))))
   }
   
+  ifEnabled <- function(checkboxValue, matrixName) {
+    if (!checkboxValue) return(NULL)
+    
+    matrixName
+  }
+  
   tablesInUI <- c(
     Target = "obsvn",
-    `target-to-source offset` = "weights",
+    `target-to-source offset` = "weightsOffset",
     Covariates = "targetValuesCovariates",
     `Coordinates & chronology` = "exportCoordinates",
+    Components = "weights",
     Sources = "source",
     `source specific offset` = "sourceOffset",
     Concentrations = "concentration"
   )
-  namesTablesToCheck <- c("Target", "target-to-source offset")
+  checkboxesInUI <- c(
+    `Include target offset` = "targetOffset",
+    `Enter Covariates` = "targetValuesShowCovariates",
+    `Coordinates & chronology` = "targetValuesShowCoordinates",
+    `Include source specific offsets` = "includeSourceOffset",
+    `Include components` = "modelWeights",
+    `Include concentrations` = "modelConcentrations"
+  )
+  
+  namesTablesToCheck <- c(
+    "Target", 
+    ifEnabled(values$targetOffset, "target-to-source offset"),
+    ifEnabled(values$targetValuesShowCovariates, "Covariates"),
+    ifEnabled(values$targetValuesShowCoordinates, "Coordinates & chronology"),
+    ifEnabled(values$modelWeights, "Components"),
+    "Sources",
+    ifEnabled(values$includeSourceOffset, "source specific offset"),
+    ifEnabled(values$modelConcentrations, "Concentrations")
+    )
   
   emptyTables <- c()
   for (i in tablesInUI[namesTablesToCheck]) {
