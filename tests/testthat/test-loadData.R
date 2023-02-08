00eb77173c2324dcd609083940c6871d0bcabf2e
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-02-08 17:36:05 +0100

    remove depricated files

--- tests/testthat/test-loadData.R
+++
@@ -1,8 +0,0 @@
-context("Load data")
-
-test_that("duplicate rownames", {
-  res <- loadData(testthat::test_path("sources.xlsx"), "xlsx", rownames = TRUE)
-
-  expect_equal(dim(res), c(48, 6))
-  expect_setequal(rownames(res), c("Plants", "TerrestrialAnimals", "MarineFish", "FreshwaterFish"))
-})
